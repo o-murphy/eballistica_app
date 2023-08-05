8f6cd3181eda8ccde508d96917226c4c4f770f03
o-murphy <thehelixpg@gmail.com>
2023-08-05 23:31:45 +0300

    init rifle_card.py, rifle_card.kv

--- kvgui/components/__init__.py
+++ kvgui/components/__init__.py
@@ -2,3 +2,4 @@ from .top_bar.top_bar import TopBar
 from .bottom_bar.bottom_bar import BottomBar
 from .ammo_list.ammo_list import AmmosList, AmmoListItem, AmmosScreen
 from .rifles_list.rifles_list import RiflesList, RifleListItem, RiflesScreen
+from .rifle_card.rifle_card import RiflesCardScreen
---
+++ kvgui/components/rifle_card/rifle_card.py
@@ -0,0 +1,9 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+
+Builder.load_file('../kvgui/components/rifle_card/rifle_card.kv')
+
+
+class RiflesCardScreen(Screen):
+    pass
