0cbad07f67201a88c109d6ce0eb33d32f7c3e541
Murphy <thehelixpg@gmail.com>
2023-08-09 14:36:17 +0300

    refactored rifle_card.py gui template

--- kvgui/components/rifle_card.py
+++ kvgui/components/rifle_card.py
@@ -1,185 +1,9 @@
-# Builder.load_string("""
-# <CardTitle>
-#     font_style: 'Subtitle1'
-#     size_hint_y: None
-#     height: self.texture_size[1]
-#
-# <FloatField>
-#     text: '0.0'
-#     input_filter: 'float'
-#
-# <MD3Card>
-#     orientation: "vertical"
-#     padding: "20dp"
-#     spacing: "5dp"
-#     # height: self.minimum_height
-#     # size_hint_y: None
-#
-# <UnitValueField>
-#     orientation: "horizontal"
-#     height: self.minimum_height
-#     size_hint_y: None
-#
-# <TwistDirSelector>
-#     orientation: "horizontal"
-#     height: self.minimum_height
-#     size_hint_y: None
-#
-# <NameField>
-#     text: 'New rifle'
-#     # hint_text: "Weapon name"
-#     helper_text: "Required field"
-#     helper_text_mode: "on_error"
-#     max_text_length: 40
-#     required: True
-# """)
-#
-#
-# class UnitValueField(BoxLayout):
-#     def __init__(self, **kwargs):
-#         super(UnitValueField, self).__init__(**kwargs)
-#         self.init_ui()
-#
-#     def init_ui(self):
-#         self.prefix = MDLabel(size_hint_x=.6)
-#         self.field = ConverMDSpinBox(size_hint_x=.3)
-#         self.suffix = MDLabel(size_hint_x=.1)
-#         self.add_widget(self.prefix)
-#         self.add_widget(self.field)
-#         self.add_widget(self.suffix)
-#
-#
-# class TwistDirSelector(BoxLayout):
-#     def __init__(self, **kwargs):
-#         super(TwistDirSelector, self).__init__(**kwargs)
-#         self.init_ui()
-#         self.bind_ui()
-#
-#     def init_ui(self):
-#         self.label = Label()
-#         self.dropdown = Button()
-#         self.dropdown.text = "Right"
-#         self.dropdown.icon = "rotate-right"
-#
-#         self.add_widget(self.label)
-#         self.add_widget(self.dropdown)
-#         # self.add_widget(MDWidget(size_hint_x=.1))
-#
-#     def bind_ui(self):
-#         self.dropdown.bind(on_release=self.show_menu)
-#
-#     def show_menu(self, *args):
-#         self.menu = MDDropdownMenu(
-#             caller=self.dropdown,
-#             items=[
-#                 {"text": "Right", "leading_icon": "rotate-right",
-#                  "on_release": lambda: self.on_menu(action="Right")},
-#                 {"text": "Left", "leading_icon": "rotate-left",
-#                  "on_release": lambda: self.on_menu(action="Left")},
-#             ],
-#         )
-#         self.menu.open()
-#
-#     def on_menu(self, action):
-#         if action == 'Right':
-#             self.dropdown.icon = "rotate-right"
-#             self.dropdown.text = 'Right'
-#         elif action == 'Left':
-#             self.dropdown.icon = "rotate-left"
-#             self.dropdown.text = 'Left'
-#         self.menu.dismiss()
-#
-#
-# class CardTitle(MDLabel):
-#     pass
-#
-# class MD3Card(MDCard):
-#     def __init__(self, **kwargs):
-#         super(MD3Card, self).__init__(**kwargs)
-#         self.init_ui()
-#
-#     def init_ui(self):
-#         self.title = CardTitle()
-#         self.add_widget(self.title)
-#
-#
-# class NameField(MDTextField):
-#     pass
-#
-#
-# class NameCard(MD3Card):
-#     def init_ui(self):
-#         super(NameCard, self).init_ui()
-#         self.title.text = 'Name:'
-#         self.name = NameField()
-#         self.add_widget(self.name)
-#
-#
-# class PropsCard(BoxLayout):
-#     def init_ui(self):
-#         super(PropsCard, self).init_ui()
-#         self.title.text = 'Properties:'
-#
-#         self.height = self.minimum_height
-#         self.size_hint_y = None
-#
-#         self.layout = MDGridLayout(cols=3)
-#
-#         self.sh_p = Label(text='Sight height', size_hint_x=0.6)
-#         self.sh_v = TextInput(size_hint_x=0.3)
-#         self.sh_s = Label(text='cm', size_hint_x=0.1)
-#
-#         self.td_p = Label(text='Twist direction', size_hint_x=0.6)
-#         self.td_v = Button(size_hint_x=0.3)
-#         self.td_s = Widget(size_hint_x=0.1)
-#
-#         self.tw_p = Label(text='Twist', size_hint_x=0.6)
-#         self.tw_v = TextInput(size_hint_x=0.3)
-#         self.tw_s = Widget(size_hint_x=0.1)
-#
-#         self.layout.add_widget(self.sh_p)
-#         self.layout.add_widget(self.sh_v)
-#         self.layout.add_widget(self.sh_s)
-#
-#         self.layout.add_widget(self.td_p)
-#         self.layout.add_widget(self.td_v)
-#         self.layout.add_widget(self.td_s)
-#
-#         self.layout.add_widget(self.tw_p)
-#         self.layout.add_widget(self.tw_v)
-#         self.layout.add_widget(self.tw_s)
-#
-#         self.add_widget(self.layout)
-#
-#
-# class InScrollBox(BoxLayout):
-#     pass
-
-
-# class RifleCardScreen(Screen):
-#     def __init__(self, **kwargs):
-#         super(RifleCardScreen, self).__init__(**kwargs)
-#         self.name = 'rifle_card'
-#         self.init_ui()
-#
-#     def init_ui(self):
-#         self.scroll = MDScrollView()
-#         self.layout = InScrollBox()
-#
-#         # self.name_card = NameCard()
-#         self.props_card = PropsCard()
-#
-#         # self.layout.add_widget(self.name_card)
-#         self.layout.add_widget(self.props_card)
-#
-#         self.scroll.add_widget(self.layout)
-#         self.add_widget(self.scroll)
-
-
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.button import MDRaisedButton, MDRectangleFlatIconButton
+from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
+from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.textfield import MDTextField
 
 from kvgui.components.spinbox import MDUnitsInput
@@ -206,6 +30,18 @@ rifle_card_helper = """
     halign: "left"
     size_hint_x: 0.1
     
+<TwistDirSelector>
+    text: 'Right'
+    size_hint_x: 0.3
+    size_hint_y: None
+    height: dp(30)
+    
+<MD3CardAbs>
+    _padding: dp(15)
+    _spacing: dp(15)
+    padding: self._padding
+    spacing: self._spacing
+
 <RifleCardScreen>
     
     MDScrollView:
@@ -216,14 +52,10 @@ rifle_card_helper = """
             padding: "20dp"
             spacing: "15dp"
                 
-            MDCard:
-                _padding: dp(15)
-                _spacing: dp(15)
+            MD3CardAbs:
                 orientation: 'horizontal'
                 size_hint_y: None
                 height: dp(60)
-                padding: self._padding
-                spacing: self._spacing
             
                 FormLabel:
                     id: name_l
@@ -235,14 +67,11 @@ rifle_card_helper = """
                     text: 'Rifle'
                     size_hint_x: 0.8
             
-            MDCard:
-                _padding: dp(15)
-                _spacing: dp(15)
+            MD3CardAbs:
                 orientation: 'vertical'
                 size_hint_y: None
                 height: dp(220)
-                padding: self._padding
-                spacing: self._spacing
+
                 
                 FormLabel:
                     id: prop_title
@@ -303,14 +132,15 @@ rifle_card_helper = """
                         id: tw_s
                         text: 'inch'
                         
-<TwistDirSelector>
-    text: 'Right'
-    size_hint_x: 0.3
-    size_hint_y: None
-    height: dp(30)
+
+
 """
 
 Builder.load_string(rifle_card_helper)
+
+
+class MD3CardAbs(MDCard):
+    pass
 
 
 class FormInput(MDTextField):
@@ -330,7 +160,39 @@ class FormSuffix(MDLabel):
 
 
 class TwistDirSelector(MDRectangleFlatIconButton):
-    pass
+    def __init__(self, **kwargs):
+        super(TwistDirSelector, self).__init__(**kwargs)
+        self.init_ui()
+        self.bind_ui()
+        self.value = "Right"  # TODO: must be Enum
+
+    def init_ui(self):
+        self.menu = MDDropdownMenu(
+            caller=self,
+            items=[
+                {"text": "Right", "leading_icon": "rotate-right",
+                 "on_release": lambda: self.on_menu(action="Right")},
+                {"text": "Left", "leading_icon": "rotate-left",
+                 "on_release": lambda: self.on_menu(action="Left")},
+            ],
+        )
+
+    def bind_ui(self):
+        self.bind(on_release=self.show_menu)
+
+    def show_menu(self, *args, **kwargs):
+        self.menu.open()
+
+    def on_menu(self, action):
+        if action == 'Right':
+            self.icon = "rotate-right"
+            self.text = 'Right'
+            self.value = action
+        elif action == 'Left':
+            self.icon = "rotate-left"
+            self.text = 'Left'
+            self.value = action
+        self.menu.dismiss()
 
 
 class RifleCardScreen(Screen):
