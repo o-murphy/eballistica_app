223fb3e79d38064e09594906af1f8557f24f1563
o-murphy <thehelixpg@gmail.com>
2023-08-20 18:51:47 +0300

    recipes sqlalchemy==2.0.0

--- recipes/sqlalchemy/__init__.py
+++ recipes/sqlalchemy/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class SQLAlchemyRecipe(CompiledComponentsPythonRecipe):
     name = 'sqlalchemy'
-    version = '2.0.20'
+    version = '2.0.0'
     url = 'https://pypi.python.org/packages/source/S/SQLAlchemy/SQLAlchemy-{version}.tar.gz'
     call_hostpython_via_targetpython = False
 
