0a183d5b24056ac16773aa303105ea45f15709cd
Murphy <thehelixpg@gmail.com>
2023-08-20 10:17:39 +0300

    specified sqlalchemy==2.0.20

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from kvgui.app import EBallisticaApp
+# from kvgui.app import EBallisticaApp
 __version__ = '0.0.5'
 
 
+# def main(argv):
+#     EBallisticaApp().run()
+
+from datatypes.dbworker import *
+
+
 def main(argv):
-    EBallisticaApp().run()
+    Worker.list_rifles()
 
 
 if __name__ == '__main__':
