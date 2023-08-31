fc04a78b1aec882e00654a87f429f32aa9e54533
Murphy <thehelixpg@gmail.com>
2023-08-31 17:18:03 +0300

    fix env paths

--- kvgui/modules/env.py
+++ kvgui/modules/env.py
@@ -5,32 +5,18 @@ import os
 
 __all__ = ['APP_DATA', 'STORAGE', 'USER_DATA']
 
-
-ANDROID_APP_DATA = '/data/data/o.murphy.eballistica'
-ANDROID_STORAGE = '/storage/emulated/0'
-ANDROID_USER_DATA = '/storage/emulated/0/Android/data/o.murphy.eballistica/files'
-
-WINDOWS_APP_DATA = os.path.join(os.environ['LocalAppData'], 'eBallistica')
-WINDOWS_STORAGE = os.path.expanduser("~\documents")
-WINDOWS_USER_DATA = os.path.expanduser("~\documents\eBallistica")
-
-UNIX_APP_DATA = os.path.expanduser("~\.eBallistica")
-UNIX_STORAGE = os.path.expanduser("~\documents")
-UNIX_USER_DATA = os.path.expanduser("~\documents\eBallistica")
-
-
 if platform == 'android':
-    APP_DATA = ANDROID_APP_DATA
-    STORAGE = ANDROID_STORAGE
-    USER_DATA = ANDROID_USER_DATA
+    APP_DATA = '/data/data/o.murphy.eballistica'
+    STORAGE = '/storage/emulated/0'
+    USER_DATA = '/storage/emulated/0/Android/data/o.murphy.eballistica/files'
 elif platform == 'win':
-    APP_DATA = WINDOWS_APP_DATA
-    STORAGE = WINDOWS_STORAGE
-    USER_DATA = WINDOWS_USER_DATA
+    APP_DATA = os.path.join(os.environ['LocalAppData'], 'eBallistica')
+    STORAGE = os.path.expanduser(r"~\documents")
+    USER_DATA = os.path.expanduser(r"~\documents\eBallistica")
 else:
-    APP_DATA = UNIX_APP_DATA
-    STORAGE = UNIX_STORAGE
-    USER_DATA = UNIX_USER_DATA
+    APP_DATA = os.path.expanduser(r"~/.eBallistica")
+    STORAGE = os.path.expanduser(r"~/documents")
+    USER_DATA = os.path.expanduser(r"~/documents\eBallistica")
 
 
 if not os.path.exists(APP_DATA):
